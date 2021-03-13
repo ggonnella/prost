f2585412b7d0ef694ebb3b4fed38a01ca41e3a19
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2021-03-13 12:29:00 +0100

    make gff_compute report unexpected events

--- scripts/computestats/counts_from_refseq_gff.py
+++ scripts/computestats/counts_from_refseq_gff.py
@@ -1,39 +1,64 @@
 #!/usr/bin/env python3
 """
 Count features of different type and class
-from a NCBI Refseq genome annotation GFF file
+from a NCBI Refseq genome annotation GFF file.
 """
 
 from collections import defaultdict
 
-def results(db):
-  counters = defaultdict(int)
-  genes = db.features_of_type("gene")
-  for g in genes:
-    children = list(db.children(g.id, level=1))
-    assert(len(children) >= 1)
-    child = children[0]
-    assert(all([child.featuretype == c.featuretype for c in children]))
-    if child.featuretype == "CDS":
-      counters["protein_coding_genes"] += 1
-    elif child.featuretype == "tRNA":
-      counters["tRNAs"] += 1
-    elif child.featuretype in ["ncRNA", "RNase_P_RNA", "SRP_RNA", "tmRNA",
-                               "antisense_RNA"]:
-      counters["ncRNAs"] += 1
-    elif child.featuretype == "rRNA":
-      product = child.attributes['product'][0]
-      if product.startswith("16S"):
-        counters["rRNA_16S"] += 1
-      elif product.startswith("5S"):
-        counters["rRNA_5S"] += 1
-      elif product.startswith("23S"):
-        counters["rRNA_23S"] += 1
-      else:
-        assert(False)
+def process_gene_feature(db, gene, counters, info):
+  children = list(db.children(gene.id, level=1))
+  if len(children) == 0:
+    info["gene_with_no_children"].append(str(gene))
+    return
+  child = children[0]
+  if not all([child.featuretype == c.featuretype for c in children]):
+    info["gene_with_heterogeneous_children_type"].append(str(gene))
+    return
+  if child.featuretype == "CDS":
+    counters["protein_coding_genes"] += 1
+  elif child.featuretype == "tRNA":
+    counters["tRNAs"] += 1
+  elif child.featuretype in ["ncRNA", "RNase_P_RNA", "SRP_RNA", "tmRNA",
+                             "antisense_RNA"]:
+    counters["ncRNAs"] += 1
+  elif child.featuretype == "rRNA":
+    product = child.attributes['product'][0]
+    if product.startswith("16S"):
+      counters["rRNA_16S"] += 1
+    elif product.startswith("5S"):
+      counters["rRNA_5S"] += 1
+    elif product.startswith("23S"):
+      counters["rRNA_23S"] += 1
     else:
-      raise ValueError(f"Feature Type under Gene: {child.featuretype} "+\
-                       f"unexpected (ID={child.id})")
-  counters["pseudogenes"] = db.count_features_of_type("pseudogene")
-  return counters
+      info["rRNA_unknown"].append(str(child))
+      assert(False)
+  elif child.featuretype == "hammerhead_ribozyme":
+    counters["hammerhead_ribozymes"] += 1
+  else:
+    info["unexpected_gene_child_feature_type"].append(str(child))
+
+def analyze(db):
+  counters = defaultdict(int)
+  info = defaultdict(list)
+  for ftr in db.all_features():
+    if len(list(db.parents(ftr.id))) == 0:
+      if ftr.featuretype == "gene":
+        process_gene_feature(db, ftr, counters, info)
+      elif ftr.featuretype == "pseudogene":
+        counters["pseudogenes"] += 1
+      elif ftr.featuretype == "region":
+        counters["n_molecules"] += 1
+      elif ftr.featuretype in ["sequence_feature"]:
+        info["generic_sequence_feature"].append(str(ftr))
+      elif ftr.featuretype == "riboswitch":
+        counters["riboswitches"] += 1
+      elif ftr.featuretype == "direct_repeat":
+        if ftr.attributes["rpt_family"][0] == "CRISPR":
+          counters["CRISPRs"] += 1
+        else:
+          info["direct_repeat_not_CRISPR"].append(str(ftr))
+      else:
+        info["top_level_feature_other_type"].append(str(ftr))
+  return counters, info
 
--- scripts/gff_compute.py
+++ scripts/gff_compute.py
@@ -6,13 +6,18 @@ Usage:
   gff_compute.py [options] <module> <gff>
 
 Arguments:
-  gff:    GFF file
-  module: python module, providing a results(db) function
-          where db is a gffutils database object;
-          the function returns a dict of results
-          with string key and values
+  gff:    GFF file (uncompressed or gzipped)
+  module: python module, which must provide a function:
+            analyze(db) => (counters, info)
+          <db>: "gffutils" database object
+          <counters>: a dict of strings => int
+                      where expected features are counted
+          <info>: a dict of string => list of strings
+                  where unexpected features or errors are stored
 
 Options:
+  --out, -o FILE       output file (default: standard output)
+  --info, -i INFO      log the info to the given file
   --verbose, -v        be verbose
   --version, -V        show script version
   --help, -h           show this help message
@@ -22,25 +27,33 @@ from docopt import docopt
 from schema import Schema, Use, Optional, Or
 import gffutils
 import os
-from lib import snake, mod
+from lib import snake, mod, valid
 
 def main(args):
   m = mod.importer(args["<module>"], args["--verbose"])
   db = gffutils.create_db(args["<gff>"], ":memory:",
                           merge_strategy="create_unique")
-  for k,v in m.results(db).items():
-    print(f"{k}\t{v}")
+  counters, info = m.analyze(db)
+  for k, v in counters.items():
+    args["--out"].write(f"{k}\t{v}\n")
+  if args["--info"]:
+    for k, v in info.items():
+      for e in v:
+        args["--info"].write(f"{k}\t{e}\n")
 
 def validated(args):
   schema = Schema({"<gff>": os.path.exists,
                    "<module>": os.path.exists,
                    "--verbose": Or(None, bool),
+                   "--out": valid.outfile_or_stdout,
+                   "--info": valid.outfile_or_none,
                    Optional(str): object})
   return schema.validate(args)
 
 if "snakemake" in globals():
   args = snake.args(snakemake,
         input=["<gff>", "<module>"],
+        output=["--info", "--out"],
         params=["--verbose"]
       )
   main(validated(args))
