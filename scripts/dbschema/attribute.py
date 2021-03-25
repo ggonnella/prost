0262afa50334d543b1218a766b5a9ea72102da09
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2021-03-25 12:22:17 +0100

    attribute, new version with named attr columns

--- scripts/dbschema/attribute.py
+++ scripts/dbschema/attribute.py
@@ -3,10 +3,13 @@
 DB Schema for tables storing attribute values.
 """
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Text, Table, inspect, select
+from sqlalchemy import Column, Integer, String, Text, Table,\
+                       inspect, select, text
+import sqlalchemy.types
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.orm import declared_attr
 from sqlalchemy_repr import PrettyRepresentableBase
+import ast
 
 Base = declarative_base(cls=PrettyRepresentableBase)
 
@@ -21,8 +24,7 @@ class AttributeDefinition(Base):
   related to the definition, in a way described in the definition.
   """
   __tablename__ = "pr_attribute_definition"
-  id = Column(Integer, primary_key=True, autoincrement=False)
-  name = Column(String(64), unique=True)
+  name = Column(String(62), primary_key=True)
   datatype = Column(String(256), nullable=False)
   definition = Column(Text(1024))
   ontology_pfx = Column(String(64))
@@ -33,246 +35,238 @@ class AttributeDefinition(Base):
 class AttributeValueMixin:
   accession = Column(String(64), primary_key = True)
   __table_args__ = utf8_cs_args
-  __tablenamepfx__ = "pr_attribute_value_t"
+  __tablepfx__ = "pr_attribute_value_t"
 
   @declared_attr
   def __tablename__(cls):
-    return cls.__tablenamepfx__ + str(cls.__tablenum__)
+    return cls.__tablepfx__ + str(cls.__tablesfx__)
 
-def _create_nth_table(n, engine):
-  klass = type("AttributeValueT"+str(n), (AttributeValueMixin, Base),
-               { "__tablenum__": n})
-  klass.metadata.tables[klass.__tablename__].create(bind=engine.engine)
+class AttributeValueTables():
 
-def nth_tablename(n):
-  """
-  Name of the n-th attribute_value table
+  @staticmethod
+  def tablename(sfx):
+    return AttributeValueMixin.__tablepfx__ + str(sfx)
 
-  Note: the function does not check if a table with such name exists.
-  """
-  pfx = AttributeValueMixin.__tablenamepfx__
-  return pfx+str(n)
+  @staticmethod
+  def normalize_suffix(sfx):
+    return str(sfx).upper()
 
-def klass(n, engine):
-  """
-  Returns the class for the n-th table.
-  """
-  return Table(nth_tablename(n), Base.metadata, autoload_with=engine)
+  @staticmethod
+  def classname(sfx):
+    return f"AttributeValueT{sfx}"
 
-def high_table_num(engine):
-  """
-  Returns the highest table number of existing tables
-  """
-  insp = inspect(engine)
-  tablenames = insp.get_table_names()
-  pfx = AttributeValueMixin.__tablenamepfx__
-  attrvaluetables = [tn for tn in tablenames if tn.startswith(pfx)]
-  if len(attrvaluetables) == 0:
-    return None
-  tablenums = [int(tn[len(pfx):]) for tn in attrvaluetables]
-  return max(tablenums)
+  def _init_attributes_maps(self):
+    a2t = {}
+    t2a = {}
+    ncols = {}
+    pfx = AttributeValueMixin.__tablepfx__
+    inspector = inspect(self.engine)
+    for tn in inspector.get_table_names():
+      if tn.startswith(pfx):
+        cnames = [c["name"] for c in inspector.get_columns(tn)]
+        sfx = tn[len(pfx):]
+        ncols[sfx] = len(cnames)
+        cnames.remove("accession")
+        anames = set(cn.rsplit("_", 1)[0] for cn in cnames)
+        t2a[sfx] = anames
+        for an in anames:
+          if an in a2t:
+            raise RuntimeError(f"Attribute {an} found in multiple tables\n"+\
+                               f"({self.tablename(a2t[an])} and {tn})")
+          a2t[an] = sfx
+    return a2t, t2a, ncols
 
-def _attrnums_range_for_table(engine, tablename):
-  cnames = [info["name"] for info in inspect(engine).get_columns(tablename)]
-  attrnums = [int(cn.split("v")[0][len("attr"):]) for cn in \
-        cnames if cn.startswith("attr") and not cn.endswith("c")]
-  if attrnums:
-    return min(attrnums), max(attrnums)
-  else:
-    return None, None
+  def __init__(self, engine):
+    self.engine = engine
+    self._a2t, self._t2a, self._ncols = self._init_attributes_maps()
 
-def nth_attrnums_range(engine, n):
-  """
-  Returns the range of value numbers of table with given table number
-  """
-  return _attrnums_range_for_table(engine, nth_tablename(n))
+  @property
+  def table_suffixes(self):
+    return list(self._t2a.keys())
 
-def _next_attrnum(engine, n):
-  h = nth_attrnums_range(engine, n)[0]
-  if h is None:
-    if n == 0: return 0
-    else: return _next_attrnum(engine, n-1)
-  else: return h+1
+  @property
+  def attribute_names(self):
+    return list(self._a2t.keys())
 
-TARGET_N_COLUMNS_FOR_TABLE = 64
+  def create_table(self, sfx):
+    """
+    Create new table with suffix <sfx>
+    """
+    sfx = self.normalize_suffix(sfx)
+    if sfx in self._t2a:
+      raise RuntimeError(f"Cannot create table: suffix {sfx} is not unique")
+    klass = type(self.classname(sfx), (AttributeValueMixin, Base),
+                 { "__tablesfx__": sfx})
+    klass.metadata.tables[klass.__tablename__].create(bind=self.engine)
+    self._t2a[sfx] = set()
+    self._ncols[sfx] = 1
 
-def _n_columns_for_table(engine, tablename):
-  return len(inspect(engine).get_columns(tablename))
+  def get_class(self, sfx):
+    """
+    Class reflecting table with suffix <sfx>
+    """
+    return Table(self.tablename(self.normalize_suffix(sfx)), Base.metadata,
+                 autoload_with=self.engine)
 
-def _new_attribute_coordinates(engine, ncols):
-  """
-  Return the table and attribute number for a new attribute
-  for which ncols value columns and a computation id column are needed.
-  Create the table if needed.
+  def new_suffix(self) -> str:
+    """
+    Get a not-yet used numerical suffix
+    """
+    i = 0
+    while True:
+      if str(i) not in self._t2a:
+        return str(i)
+      i += 1
 
-  A new table is created if the last created table has already value columns
-  and the sum of the existing and new columns is higher than
-  TARGET_N_COLUMNS_FOR_TABLE.
-  """
-  table_num = high_table_num(engine)
-  attr_num = _next_attrnum(engine, table_num)
-  nprevcols = _n_columns_for_table(engine, nth_tablename(table_num))
-  if nprevcols > 1 and nprevcols + ncols + 1 > TARGET_N_COLUMNS_FOR_TABLE:
-    table_num += 1
-    _create_nth_table(table_num, engine)
-  return table_num, attr_num
+  TARGET_N_COLUMNS = 64
+  C_ID_TYPE = sqlalchemy.types.BINARY(16)
 
-def _create_nth_attribute_columns(engine, datatypes, table_num, n):
-  tablename = nth_tablename(table_num)
-  if len(datatypes) == 1:
-    coldefs = [f"attr{n}v {datatypes[0]}"]
-  else:
-    coldefs = [f"attr{n}v{i} {datatypes[i]}" for i in range(len(datatypes))]
-  coldefs.append(f"attr{n}c {C_ID_TYPE}")
-  coldefs = ", ".join(coldefs)
-  engine.execute(f"ALTER TABLE {tablename} "+
-                 f"ADD COLUMN ({coldefs})")
+  def _place_for_new_attr(self, ncols):
+    """
+    Table suffix for a new attribute for which ncols value columns and
+    a computation id column are needed. Creates the table if needed.
 
-def table_num_for_nth_attr(engine, n):
-  """
-  Find number of table where the nth attribute is stored
+    A new table is created if all tables have already value columns
+    and the sum of the existing and new columns is higher than TARGET_N_COLUMNS.
+    """
+    for sfx in self.table_suffixes:
+      if (self._ncols[sfx] == 1) or \
+         (self._ncols[sfx] + ncols + 1 <= self.TARGET_N_COLUMNS):
+        return sfx
+    sfx = self.new_suffix()
+    self.create_table(sfx)
+    return sfx
 
-  A linear search is performed. Null is returned if n is larger
-  than the largest current attribute number.
-  """
-  assert(n >= 0)
-  table_num = high_table_num(engine)
-  l, h = nth_attrnums_range(engine, table_num)
-  while h is None:
-    if table_num == 0:
-      return None
-    else:
-      table_num -= 1
-      l, h = nth_attrnums_range(engine, table_num)
-  if n > h:
-    return None
-  else:
-    while n < l:
-      table_num -= 1
-      assert(table_num >= 0)
-      l = nth_attrnums_range(engine, table_num)[0]
-    return table_num
+  @staticmethod
+  def _vcolnames(a_name, nelems):
+    if nelems == 1: return [f"{a_name}_v"]
+    else:           return [f"{a_name}_v{i}" for i in range(nelems)]
 
-def _parse_datatype_def(datatype_def):
-  result = []
-  for elem in datatype_def.split(","):
-    if elem.endswith("]"):
-      dt, n = elem.split("[")
-      n = int(n[:-1])
-      assert(n > 1)
-      for i in range(n):
-        result.append(dt)
-    else:
-      result.append(elem)
-  return result
+  @staticmethod
+  def _ccolname(a_name):
+    return a_name+"_c"
 
-def _get_table_numbers(engine):
-  tablenames = inspect(engine).get_table_names()
-  pfx = AttributeValueMixin.__tablenamepfx__
-  attrvaluetables = [tn for tn in tablenames if tn.startswith(pfx)]
-  return [int(tn[len(pfx):]) for tn in attrvaluetables]
+  def _create_attr_cols(self, a_name, a_datatypes, t_sfx):
+    if a_name in self._a2t:
+      raise RuntimeError(f"Attribute {a_name} exists already in table"+\
+                         self._a2t[a_name])
+    colnames = self._vcolnames(a_name, len(a_datatypes))
+    coldefs = [f"{cn} {dt}" for cn, dt in zip(colnames, a_datatypes)]
+    coldefs.append(self._ccolname(a_name) + " " + str(self.C_ID_TYPE))
+    coldefs_str = ", ".join(coldefs)
+    tn = self.tablename(t_sfx)
+    with self.engine.begin() as tr:
+      tr.execute(text(f"ALTER TABLE {tn} ADD COLUMN ({coldefs_str})"))
+    self._t2a[t_sfx].add(a_name)
+    self._a2t[a_name] = t_sfx
+    self._ncols[t_sfx] += len(coldefs)
 
-def _get_attr_numbers(engine, n_tables):
-  attrnums = []
-  for tnum in range(n_tables):
-    cnames = [info["name"] for info in \
-        inspect(engine).get_columns(nth_tablename(tnum))]
-    t_attrnums = set(int(cn.split("v")[0][len("attr"):]) for cn in \
-        cnames if cn.startswith("attr") and not cn.endswith("c"))
-    attrnums.append(t_attrnums)
-  return attrnums
+  def _destroy_attr_cols(self, a_name, a_datatypes, t_sfx):
+    colnames = self._vcolnames(a_name, len(a_datatypes))
+    colnames.append(self._ccolname(a_name))
+    tn = self.tablename(t_sfx)
+    with self.engine.begin() as tr:
+      dstr = ", ".join([f"DROP COLUMN {cn}" for cn in colnames])
+      tr.execute(text(f"ALTER TABLE {tn} {dstr}"))
+    self._t2a[t_sfx].remove(a_name)
+    del self._a2t[a_name]
+    self._ncols[t_sfx] -= len(colnames)
 
-def _check_numbers(l, low=0):
-  l.sort()
-  prev = low-1
-  for e in l:
-    if e != prev+1:
-      return False
-    prev = e
-  return True
+  @staticmethod
+  def _parse_datatype_def(datatype_def):
+    result = []
+    for elem in datatype_def.split(";"):
+      if elem.endswith("]"):
+        dts, n = elem.split("[")
+        n = int(n[:-1])
+        assert(n > 1)
+      else:
+        dts = elem
+        n = 1
+      if dts.endswith(")"):
+        dts_type, dts_sfx = elem.split("(")
+        dts_params = [ast.literal_eval(p.strip()) \
+                        for p in dts_sfx[:-1].split(",")]
+      else:
+        dts_type = dts
+        dts_params = []
+      if not hasattr(sqlalchemy.types, dts_type):
+        raise ValueError("Unknown datatype in attribute datatype "+\
+                         f"definition {dts_type}")
+      dt = getattr(sqlalchemy.types, dts_type)(*dts_params)
+      result += [dt]*n
+    return result
 
-C_ID_TYPE = "BINARY(16)"
+  def create_attribute(self, name, datatype_def, **kwargs):
+    """
+    Create a new attribute record in the attribute_definition table
+    and reserve space in the attribute_values tables for storing
+    the attribute values and computation IDs.
 
-def equiv(datatype1, datatype2):
-  # not implemented, this should check if two datatype
-  # definitions are equivalent, e.g. INT(11) == INTEGER
-  return True
+    Datatype definition, one of:
+    - scalar: datatype, including any parameter in ()
+    - array: datatype, followed by [<n>], with n integer > 0
+    - list of scalar and/or array, semicolon-sep, wo spaces
 
-def _check_column(k, edt, tcols, checked_colnames, desc):
-  if not k in tcols:
-    raise ValueError(f"Missing column {k} ({desc})")
-  checked_colnames.append(k)
-  dt = tcols[k]
-  if not equiv(dt, edt):
-    raise ValueError(f"Wrong datatype for column {k} ({desc}): "+\
-                     f"found {dt}, expected {edt}")
+    Datatype is thereby any of the types defined
+    in the sqlAlchemy.types module.
 
-def _check_attribute_columns(engine, anum, tcols, datatypes, checked_colnames):
-  k = f"attr{anum}c"
-  _check_column(f"attr{anum}c", C_ID_TYPE, tcols, checked_colnames,
-                f"computation ID of attribute {anum}")
-  if len(datatypes) == 1:
-    _check_column(f"attr{anum}v", datatypes[0], tcols, checked_colnames,
-                  f"value column of attribute {anum}")
-  else:
-    for enum, edt in enumerate(datatypes):
-      _check_column(f"attr{anum}v{enum}", edt, tcols, checked_colnames,
-                    f"value element {enum} of attribute {anum}")
+    e.g. Boolean[8];Integer;String(12);BINARY(16)[2]
+    """
+    a_datatypes = self._parse_datatype_def(datatype_def)
+    t_sfx = self._place_for_new_attr(len(a_datatypes))
+    adef = AttributeDefinition(name = name,
+                               datatype = datatype_def, **kwargs)
+    session = sessionmaker(bind=self.engine)()
+    session.add(adef)
+    self._create_attr_cols(name, a_datatypes, t_sfx)
+    session.commit()
 
-def check_consistency(engine):
-  Session = sessionmaker(bind=engine)
-  session = Session()
-  tnums = _get_table_numbers(engine)
-  if not _check_numbers(tnums):
-    raise ValueError(f"The table numbers sequence is corrupted:\n{tnums}")
-  anums = _get_attr_numbers(engine, len(tnums))
-  expmin = 0
-  allt_anums = []
-  for tnum, t_anums in enumerate(anums):
-    if t_anums:
-      if not _check_numbers(list(t_anums), expmin):
-        raise ValueError(f"The attribute numbers sequence is corrupted:\n{anums}")
-      expmin = max(t_anums)+1
-    allt_anums += list(t_anums)
-  unexpected_adef = session.execute(select(AttributeDefinition).where(
-                      AttributeDefinition.id.notin_(allt_anums))).all()
-  if unexpected_adef:
-    raise ValueError("Some attribute definitions do not correspond to "+
-                     "columns in the attribute_value tables:\n"+
-                     f"{unexpected_adef}")
-  for tnum in tnums:
-    tcols = {info[0]: info[1] for info in \
-        engine.execute(f"DESCRIBE {nth_tablename(tnum)}").all()}
-    checked_colnames = ["accession"]
-    for anum in anums[tnum]:
-      adef = session.execute(\
+  def destroy_attribute(self, name):
+    session = sessionmaker(bind=self.engine)()
+    adef=session.query(AttributeDefinition).filter(
+        AttributeDefinition.name==name).first()
+    if name not in self._a2t:
+      raise RuntimeError(f"Attribute {name} does not exist")
+    self._destroy_attr_cols(name, self._parse_datatype_def(adef.datatype),
+                            self._a2t[name])
+    session.delete(adef)
+    session.commit()
+
+  @staticmethod
+  def _check_column(k, edt, cols, desc):
+    if not k in cols:
+      raise ValueError(f"Missing column {k} ({desc})")
+    dt = cols[k]["type"]
+    if str(dt) != str(edt):
+      raise ValueError(f"Wrong datatype for column {k} ({desc}): "+\
+                       f"found {dt}, expected {edt}")
+
+  def check_consistency(self):
+    session = sessionmaker(bind=self.engine)()
+    inspector = inspect(self.engine)
+    self._a2t, self._t2a, self._ncols = self._init_attributes_maps()
+    unexpected_adef = session.execute(select(AttributeDefinition).where(
+                      AttributeDefinition.name.notin_(
+                        self.attribute_names))).all()
+    if unexpected_adef:
+      raise ValueError("Some attribute definitions do not correspond to "+
+                       "columns in the attribute_value tables:\n"+
+                       f"{unexpected_adef}")
+    for sfx in self.table_suffixes:
+      cols = {c["name"]: c for c in inspector.get_columns(self.tablename(sfx))}
+      for aname in self._t2a[sfx]:
+        adef = session.execute(\
           select(AttributeDefinition).\
-            where(AttributeDefinition.id==anum)).one()[0]
-      datatypes = _parse_datatype_def(adef.datatype)
-      _check_attribute_columns(engine, anum, tcols, datatypes, checked_colnames)
-    unexpected_cols = set(tcols.keys())-set(checked_colnames)
-    if unexpected_cols:
-      raise ValueError(f"Unexpected columns in table {tnum}: {unexpected_cols}")
-
-def create_attribute(engine, name, datatype_def, **kwargs):
-  """
-  Create a new attribute record in the attribute_definition table
-  and reserve space in the attribute_values tables for storing
-  the attribute values and computation IDs.
-
-  Datatype definition, one of:
-  - scalar: SQL datatype, including any parameter in ()
-  - array: SQL datatype, followed by [<n>], with n integer > 0
-  - struct: list of scalar and/or array, comma-sep, wo spaces
-
-  e.g. TINYINT(1)[8],INTEGER,VARCHAR,CHAR(255)[2]
-  """
-  datatypes = _parse_datatype_def(datatype_def)
-  tn, n = _new_attribute_coordinates(engine, len(datatypes))
-  _create_nth_attribute_columns(engine, datatypes, tn, n)
-  adef = AttributeDefinition(id = n, name = name,
-                             datatype = datatype_def, **kwargs)
-  Session = sessionmaker(bind=engine)
-  session = Session()
-  session.add(adef)
-  session.commit()
+            where(AttributeDefinition.name==aname)).one()[0]
+        self._check_column(self._ccolname(aname), self.C_ID_TYPE, cols,
+                      f"computation ID of attribute {aname}")
+        datatypes = self._parse_datatype_def(adef.datatype)
+        if len(datatypes) == 1:
+          self._check_column(self._vcolnames(aname, 1)[0], datatypes[0], cols,
+                        f"value column of attribute {aname}")
+        else:
+          vcolnames = self._vcolnames(aname, len(datatypes))
+          for i, edt in enumerate(datatypes):
+            self._check_column(vcolnames[i], edt, cols,
+                          f"{i} element of value of attribute {aname}")
